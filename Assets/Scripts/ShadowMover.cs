815b62041b02e21bd4fdbbb2763c48bfe213f7a0
Jason <jasonxiii97@gmail.com>
2024-02-01 16:17:21 +0100

    Add a script for the shadow mover

---
+++ Assets/Scripts/ShadowMover.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Shadow))]
+public class ShadowMover : MonoBehaviour
+{
+	[Min(0.01f)] public float oscillation = 1f, periodOfOscillation = 3f;
+
+	private Shadow shadow;
+	private Vector2 startDistance;
+
+	private void Awake() => shadow = GetComponent<Shadow>();
+	private void Start() => startDistance = shadow.effectDistance;
+	private void Update() => shadow.effectDistance = EffectDistance();
+	private float Angle() => Time.time*Mathf.PI / periodOfOscillation*2;
+	private float MovementX(float angle) => Mathf.Sin(-angle)*oscillation;
+	private float MovementY(float angle) => Mathf.Sin(angle)*oscillation;
+
+	private Vector2 EffectDistance()
+	{
+		float angle = Angle();
+		float x = startDistance.x + MovementX(angle);
+		float y = startDistance.y + MovementY(angle);
+
+		return new Vector2(x, y);
+	}
+}
