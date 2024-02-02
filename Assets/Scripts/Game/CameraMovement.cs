49a38c480104913175e712b0c38da4f7d9e5e7f6
Jason <jasonxiii97@gmail.com>
2024-02-02 13:38:47 +0100

    Add a linear interpolation to the camera movement

--- Assets/Scripts/Game/CameraMovement.cs
+++ Assets/Scripts/Game/CameraMovement.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 	public GameObject target;
+	[Min(0.01f)] public float speed = 5f;
 
 	private Vector3 offset;
 	
 	private void Start() => offset = transform.position - target.transform.position;
-	private void LateUpdate() => transform.position = target.transform.position + offset;
+	
+	private void FixedUpdate()
+	{
+		Vector3 lerpedPosition = Vector3.Lerp(transform.position, target.transform.position + offset, Time.fixedDeltaTime*speed);
+		
+		transform.position = lerpedPosition;
+	}
 }
