244b13f107da40b9e94035d8ae5012c17eeaf4a7
Jason <jasonxiii97@gmail.com>
2024-02-03 11:53:58 +0100

    Make the player temporarily frozen after respawn

--- Assets/Game/Scripts/Game/PlayerRespawner.cs
+++ Assets/Game/Scripts/Game/PlayerRespawner.cs
@@ -4,12 +4,27 @@ public class PlayerRespawner : MonoBehaviour
 {
 	public Transform respawnPoint;
 	public float minimumY = -10f;
+	[Min(0.01f)] public float freezeDuration = 1f;
+
+	private Rigidbody rb;
+
+	private void Awake() => rb = GetComponent<Rigidbody>();
 
 	private void Update()
 	{
 		if(transform.position.y <= minimumY)
 		{
-			transform.position = respawnPoint.position;
+			OnRespawn();
 		}
 	}
+
+	private void OnRespawn()
+	{
+		transform.position = respawnPoint.position;
+		rb.isKinematic = true;
+
+		Invoke(nameof(DisableKinematicBody), freezeDuration);
+	}
+
+	private void DisableKinematicBody() => rb.isKinematic = false;
 }
