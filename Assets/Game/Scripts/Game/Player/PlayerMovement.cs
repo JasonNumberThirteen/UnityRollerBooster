4e5c38d99c426e807d141085e83eb46cecc6b77d
Jason <jasonxiii97@gmail.com>
2024-02-03 17:25:40 +0100

    Connect the player input with the player movement

--- Assets/Game/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Game/Player/PlayerMovement.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
 	[Min(0f)] public float movementSpeed = 10f;
 
+	private PlayerInput input;
 	private Rigidbody rb;
-	private Vector2 movement;
 
-	private void Awake() => rb = GetComponent<Rigidbody>();
-	private void OnMove(InputValue iv) => movement = iv.Get<Vector2>();
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		input = GetComponent<PlayerInput>();
+	}
 
 	private void FixedUpdate()
 	{
-		Vector3 movementVector = new Vector3(movement.x, 0.0f, movement.y);
+		Vector2 movement = input.MovementVector;
+		Vector3 direction = new Vector3(movement.x, 0.0f, movement.y);
 
-		rb.AddForce(movementVector*movementSpeed);
+		rb.AddForce(direction*movementSpeed);
 	}
 }
