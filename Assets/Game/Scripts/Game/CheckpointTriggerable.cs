cb03af186a6271e5e5280d52d2da68f581816a70
Jason <jasonxiii97@gmail.com>
2024-02-04 13:48:34 +0100

    Add respawn point for the checkpoint triggerable

--- Assets/Game/Scripts/Game/CheckpointTriggerable.cs
+++ Assets/Game/Scripts/Game/CheckpointTriggerable.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class CheckpointTriggerable : MonoBehaviour, ITriggerable
 {
+	public Transform respawnPoint;
+	
 	public void TriggerOnTrigger(GameObject sender)
 	{
 		if(sender.TryGetComponent(out PlayerRespawner pr))
 		{
-			pr.respawnPoint = transform;
+			pr.respawnPoint = respawnPoint;
 		}
 	}
 }
