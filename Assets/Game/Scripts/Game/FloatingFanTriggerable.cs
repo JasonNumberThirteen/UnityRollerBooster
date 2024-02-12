e5e62faf7689621fa94e88d6c2eaf8835fbe3ea7
Jason <jasonxiii97@gmail.com>
2024-02-12 14:33:02 +0100

    Adjust physics drag by the floating fan triggerable

--- Assets/Game/Scripts/Game/FloatingFanTriggerable.cs
+++ Assets/Game/Scripts/Game/FloatingFanTriggerable.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class FloatingFanTriggerable : MonoBehaviour, ITriggerEnter, ITriggerExit
 {
 	public Vector3 pushForce;
+	[Min(0f)] public float drag;
 
 	public void TriggerOnTriggerEnter(GameObject sender)
 	{
 		if(sender.TryGetComponent(out ConstantForce cf))
 		{
 			SetForce(cf, pushForce);
+		}
+
+		if(sender.TryGetComponent(out Rigidbody rb))
+		{
+			SetDrag(rb, drag);
 		}
 	}
 
@@ -18,7 +24,13 @@ public class FloatingFanTriggerable : MonoBehaviour, ITriggerEnter, ITriggerExit
 		{
 			SetForce(cf, Vector3.zero);
 		}
+
+		if(sender.TryGetComponent(out Rigidbody rb))
+		{
+			SetDrag(rb, 0);
+		}
 	}
 
 	private void SetForce(ConstantForce cf, Vector3 force) => cf.force = force;
+	private void SetDrag(Rigidbody rb, float drag) => rb.drag = drag;
 }
