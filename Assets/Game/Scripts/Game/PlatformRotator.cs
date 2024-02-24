a23fd81f9ac3c80f7d62e3dbc5bd26ba5e89cfa1
Jason <jasonxiii97@gmail.com>
2024-02-24 09:42:13 +0100

    Adjust the Min attribute for the variable in the platform rotator

--- Assets/Game/Scripts/Game/PlatformRotator.cs
+++ Assets/Game/Scripts/Game/PlatformRotator.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlatformRotator : MonoBehaviour
 {
-	[Min(0.01f)] public float speed = 10f;
+	[Min(0f)] public float speed = 10f;
 
 	private void Update() => transform.Rotate(speed*Time.deltaTime*Vector3.up);
 }
