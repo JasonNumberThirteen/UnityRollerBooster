322cd24cd98c0e08092118632bd664525407659e
Jason <jasonxiii97@gmail.com>
2024-02-24 09:46:44 +0100

    Add setting rotation direction in the platform rotator

--- Assets/Game/Scripts/Game/PlatformRotator.cs
+++ Assets/Game/Scripts/Game/PlatformRotator.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class PlatformRotator : MonoBehaviour
 {
 	[Min(0f)] public float speed = 10f;
+	public bool inversedDirection;
 
-	private void Update() => transform.Rotate(speed*Time.deltaTime*Vector3.up);
+	private void Update()
+	{
+		int direction = inversedDirection ? -1 : 1;
+		
+		transform.Rotate(direction*speed*Time.deltaTime*Vector3.up);
+	}
 }
