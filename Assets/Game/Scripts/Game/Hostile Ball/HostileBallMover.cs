b00e498ead710c2ccf114955de46bcb2a6c0fb36
Jason <jasonxiii97@gmail.com>
2024-02-26 18:55:34 +0100

    Remove redundant code from the hostile ball mover

--- Assets/Game/Scripts/Game/Hostile Ball/HostileBallMover.cs
+++ Assets/Game/Scripts/Game/Hostile Ball/HostileBallMover.cs
@@ -1,70 +1,22 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(HostileBallPatrolPathBuilder))]
 public class HostileBallMover : MonoBehaviour
 {
 	[Min(0f)] public float speed = 10f;
-	[Min(0.1f)] public float distanceTolerance = 1f;
 
 	public Transform Target {get; set;}
 	
 	private Rigidbody rb;
-	private int pathPointIndex = 0;
-	private bool changedTarget = false;
-	private HostileBallPatrolPathBuilder patrolPathBuilder;
 
-	private void SetInitialPosition() => transform.position = CurrentPathPointPosition();
-	private void UpdateTargetAsNextPathPoint() => Target = patrolPathBuilder.Path[pathPointIndex];
-	private Vector3 CurrentPathPointPosition() => patrolPathBuilder.Path[pathPointIndex].position;
+	private void Awake() => rb = GetComponent<Rigidbody>();
+	private void FixedUpdate() => AddForceTowardsTarget();
 	private Vector3 PositionHeading() => (Target.position - transform.position).normalized;
-	private bool ReachedPosition() => Vector3.Distance(transform.position, Target.position) <= distanceTolerance;
-
-	private void Awake()
-	{
-		rb = GetComponent<Rigidbody>();
-		patrolPathBuilder = GetComponent<HostileBallPatrolPathBuilder>();
-	}
-
-	private void Start()
-	{
-		SetInitialPosition();
-		UpdateTargetAsNextPathPoint();
-	}
-
-	private void FixedUpdate()
-	{
-		if(!ReachedPosition())
-		{
-			AddForceTowardsTarget();
-			DisableTargetChange();
-		}
-		else if(!changedTarget)
-		{
-			SetNextPathPoint();
-		}
-	}
 
 	private void AddForceTowardsTarget()
 	{
 		Vector3 heading = PositionHeading();
 
 		rb.AddForce(heading*speed);
-	}
-
-	private void DisableTargetChange()
-	{
-		if(changedTarget)
-		{
-			changedTarget = false;
-		}
-	}
-
-	private void SetNextPathPoint()
-	{
-		pathPointIndex = (pathPointIndex + 1) % patrolPathBuilder.amountOfPoints;
-		changedTarget = true;
-
-		UpdateTargetAsNextPathPoint();
 	}
 }
