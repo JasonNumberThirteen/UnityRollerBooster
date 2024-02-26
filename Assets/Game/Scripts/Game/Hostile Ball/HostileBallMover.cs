eb22af29b26fd1866865633207485cde7b806c93
Jason <jasonxiii97@gmail.com>
2024-02-26 18:30:53 +0100

    Remove redundant code from the hostile ball mover

--- Assets/Game/Scripts/Game/Hostile Ball/HostileBallMover.cs
+++ Assets/Game/Scripts/Game/Hostile Ball/HostileBallMover.cs
@@ -1,54 +1,27 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(HostileBallPatrolPathBuilder))]
 public class HostileBallMover : MonoBehaviour
 {
-	public Transform patrolPoint;
-	[Min(2)] public int amountOfPoints = 2;
 	[Min(0f)] public float speed = 10f;
-	[Min(1f)] public float radius = 1f;
 	[Min(0.1f)] public float distanceTolerance = 1f;
 	
 	private Rigidbody rb;
-	private Transform[] path;
 	private int pathPointIndex = 0;
 	private bool changedTarget = false;
+	private HostileBallPatrolPathBuilder patrolPathBuilder;
 
-	private void Awake() => rb = GetComponent<Rigidbody>();
+	private void Start() => SetInitialPosition();
 	private void SetInitialPosition() => transform.position = CurrentPathPointPosition();
-	private Vector3 CurrentPathPointPosition() => path[pathPointIndex].position;
+	private Vector3 CurrentPathPointPosition() => patrolPathBuilder.Path[pathPointIndex].position;
 	private Vector3 PositionHeading(Vector3 position) => (position - transform.position).normalized;
 	private bool ReachedPosition(Vector3 position) => Vector3.Distance(transform.position, position) <= distanceTolerance;
 
-	private void Start()
+	private void Awake()
 	{
-		GeneratePointsAroundPatrolPoint();
-		SetInitialPosition();
-	}
-
-	private void GeneratePointsAroundPatrolPoint()
-	{
-		path = new Transform[amountOfPoints];
-		
-		for (int i = 1; i <= amountOfPoints; ++i)
-		{
-			GameObject pathPoint = new GameObject("Path Point #" + i);
-			
-			pathPoint.transform.SetParent(patrolPoint);
-
-			pathPoint.transform.localPosition = PathPointPosition(i);
-			path[i - 1] = pathPoint.transform;
-		}
-	}
-
-	private Vector3 PathPointPosition(int index)
-	{
-		float radians = index*Mathf.PI*2 / amountOfPoints;
-		float x = Mathf.Sin(radians)*radius;
-		float y = patrolPoint.position.y;
-		float z = Mathf.Cos(radians)*radius;
-
-		return new Vector3(x, y, z);
+		rb = GetComponent<Rigidbody>();
+		patrolPathBuilder = GetComponent<HostileBallPatrolPathBuilder>();
 	}
 
 	private void FixedUpdate()
@@ -83,17 +56,7 @@ public class HostileBallMover : MonoBehaviour
 
 	private void SetNextPathPoint()
 	{
-		pathPointIndex = (pathPointIndex + 1) % amountOfPoints;
+		pathPointIndex = (pathPointIndex + 1) % patrolPathBuilder.amountOfPoints;
 		changedTarget = true;
-	}
-	
-	private void OnDrawGizmos()
-	{
-		Gizmos.color = Color.red;
-
-		if(patrolPoint != null)
-		{
-			Gizmos.DrawWireSphere(patrolPoint.position, radius);
-		}
 	}
 }
